2f9e7f9a35514058447320e922d7f673c577a64f
kagiso008 <69651224+kagiso008@users.noreply.github.com>
2024-06-26 18:11:29 +0200

    picture can be changed on ios

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const Login(title: 'CV BUILDER'),
+      home: const ProfilePage(title: 'CV BUILDER'),
       debugShowCheckedModeBanner: false,
     );
   }
--- lib/profilepage.dart
+++ lib/profilepage.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:email_validator/email_validator.dart';
-import 'package:firebase_database/firebase_database.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:cv_builder/homepage.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'dart:io';
 
 class ProfilePage extends StatefulWidget {
   const ProfilePage({super.key, required this.title});
@@ -13,38 +17,194 @@ class ProfilePage extends StatefulWidget {
   State<ProfilePage> createState() => _ProfilePageState();
 }
 
-void _getUserDetails() async {
-  final FirebaseAuth auth = FirebaseAuth.instance;
-  final FirebaseFirestore firestore = FirebaseFirestore.instance;
-  User? user = auth.currentUser;
-  String userId = user?.uid ?? '';
-  DocumentReference userDocument = firestore.collection('users').doc(userId);
-
-  userDocument.get().then((DocumentSnapshot document) {
-    if (document.exists) {
-      print('User Data: ${document.data()}');
-      print(document['displayName']);
-      print(document['bio']);
-    } else {
-      print('No user data found');
-    }
+void _updateUserDetails(
+    String fullName, String email, String biography, String password) {
+  final user = FirebaseAuth.instance.currentUser;
+  final CollectionReference<Map<String, dynamic>> usersRef =
+      FirebaseFirestore.instance.collection('users');
+  usersRef.doc(user?.uid).set({
+    'id': user?.uid,
+    'email': email,
+    'password': password,
+    'displayName': fullName,
+    'photoUrl': user?.photoURL,
+    'bio': biography,
   });
 }
 
 class _ProfilePageState extends State<ProfilePage> {
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  final FirebaseStorage _storage = FirebaseStorage.instance;
+  final ImagePicker _picker = ImagePicker();
   final FirebaseAuth auth = FirebaseAuth.instance;
   final FirebaseFirestore firestore = FirebaseFirestore.instance;
   final _formKey = GlobalKey<FormState>();
+  final _formKey2 = GlobalKey<FormState>();
   TextEditingController emailController = TextEditingController();
   TextEditingController passwordController = TextEditingController();
+  TextEditingController confirmPasswordController = TextEditingController();
+  TextEditingController confirmEmailController = TextEditingController();
   TextEditingController usernameController = TextEditingController();
   TextEditingController bioController = TextEditingController();
+  String _imageUrl = '';
 
   @override
   void initState() {
     super.initState();
     fetchUserData();
+    _loadUserProfile();
   }
+
+  Future<void> _loadUserProfile() async {
+    User? user = auth.currentUser;
+
+    if (user != null) {
+      DocumentSnapshot userDoc =
+          await _firestore.collection('users').doc(user.uid).get();
+      setState(() {
+        _imageUrl = userDoc['photoUrl'] ?? '';
+      });
+    }
+  }
+
+  Future<void> _pickAndUploadImage() async {
+    final pickedFile = await _picker.pickImage(source: ImageSource.gallery);
+
+    if (pickedFile != null) {
+      File file = File(pickedFile.path);
+      User? user = auth.currentUser;
+
+      if (user != null) {
+        try {
+          // Upload image to Firebase Storage
+          String filePath = 'profilePictures/${user.uid}.png';
+          await _storage.ref(filePath).putFile(file);
+          String downloadUrl = await _storage.ref(filePath).getDownloadURL();
+
+          // Update image URL in Firestore
+          await _firestore
+              .collection('users')
+              .doc(user.uid)
+              .update({'photoUrl': downloadUrl});
+
+          setState(() {
+            _imageUrl = downloadUrl;
+          });
+        } catch (e) {
+          print('Error uploading image: $e');
+        }
+      }
+    }
+  }
+
+  Future openDialog() => showDialog(
+        context: context,
+        builder: (context) => AlertDialog(
+          title: const Text('please enter your log in details'),
+          content: Form(
+            key: _formKey2,
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                TextFormField(
+                  controller: confirmEmailController,
+                  decoration: const InputDecoration(labelText: 'Email'),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter your email';
+                    }
+                    return null;
+                  },
+                ),
+                TextFormField(
+                  controller: confirmPasswordController,
+                  decoration: InputDecoration(labelText: 'password'),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter your current password';
+                    }
+                    return null;
+                  },
+                ),
+              ],
+            ),
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text('Cancel'),
+            ),
+            ElevatedButton(
+              onPressed: () async {
+                if (_formKey2.currentState?.validate() ?? false) {
+                  await FirebaseAuth.instance.currentUser?.reload();
+                  if (EmailValidator.validate(confirmEmailController.text)) {
+                    if (confirmEmailController.text.isNotEmpty) {
+                      try {
+                        //showLoaderDialog(context);
+                        final credential = await FirebaseAuth.instance
+                            .signInWithEmailAndPassword(
+                                email: confirmEmailController.text,
+                                password: confirmPasswordController.text);
+                        Navigator.of(context).pop();
+                        Route route = MaterialPageRoute(
+                            builder: (context) => const HomePage(
+                                  title: "Create CV",
+                                ));
+                        Navigator.push(context, route);
+                      } on FirebaseAuthException catch (e) {
+                        if (e.code == 'user-not-found') {
+                          Fluttertoast.showToast(
+                              msg: "No user found for that email.",
+                              toastLength: Toast.LENGTH_SHORT,
+                              gravity: ToastGravity.BOTTOM,
+                              timeInSecForIosWeb: 2,
+                              backgroundColor:
+                                  const Color.fromARGB(255, 0, 0, 0),
+                              textColor: Colors.white,
+                              fontSize: 16.0);
+                        } else if (e.code == 'wrong-password') {
+                          Fluttertoast.showToast(
+                              msg: "Wrong password provided for that user.",
+                              toastLength: Toast.LENGTH_SHORT,
+                              gravity: ToastGravity.BOTTOM,
+                              timeInSecForIosWeb: 2,
+                              backgroundColor:
+                                  const Color.fromARGB(255, 0, 0, 0),
+                              textColor: Colors.white,
+                              fontSize: 16.0);
+                        }
+                      }
+                    } else {
+                      Fluttertoast.showToast(
+                          msg: "Please enter password",
+                          toastLength: Toast.LENGTH_SHORT,
+                          gravity: ToastGravity.BOTTOM,
+                          timeInSecForIosWeb: 2,
+                          backgroundColor: const Color.fromARGB(255, 0, 0, 0),
+                          textColor: Colors.white,
+                          fontSize: 16.0);
+                    }
+                  } else {
+                    Fluttertoast.showToast(
+                        msg: "Please enter valid email",
+                        toastLength: Toast.LENGTH_SHORT,
+                        gravity: ToastGravity.BOTTOM,
+                        timeInSecForIosWeb: 2,
+                        backgroundColor: const Color.fromARGB(255, 0, 0, 0),
+                        textColor: Colors.white,
+                        fontSize: 16.0);
+                  }
+                  Navigator.of(context).pop();
+                }
+              },
+              child: Text('Submit'),
+            ),
+          ],
+        ),
+      );
 
   Future<void> fetchUserData() async {
     User? user = auth.currentUser;
@@ -98,14 +258,19 @@ class _ProfilePageState extends State<ProfilePage> {
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
-                SizedBox(
-                  height: 200,
-                  width: 200,
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(200),
-                    child: const Image(
-                        image: AssetImage('assets/images/profile_image.jpg')),
-                  ),
+                _imageUrl.isNotEmpty
+                    ? CircleAvatar(
+                        radius: 50,
+                        backgroundImage: NetworkImage(_imageUrl),
+                      )
+                    : const CircleAvatar(
+                        radius: 50,
+                        child: Icon(Icons.person, size: 50),
+                      ),
+                const SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: _pickAndUploadImage,
+                  child: const Text('Change Profile Picture'),
                 ),
                 Padding(
                   padding:
@@ -177,8 +342,20 @@ class _ProfilePageState extends State<ProfilePage> {
                   child: Center(
                     child: ElevatedButton(
                       onPressed: () async {
-                        _getUserDetails();
-                        _submitForm();
+                        openDialog();
+                        _updateUserDetails(
+                            usernameController.text,
+                            emailController.text,
+                            bioController.text,
+                            passwordController.text);
+                        User? user = auth.currentUser;
+                        if (user != null) {
+                          //await user?.reauthenticateWithCredential(credential);
+                          await user?.updatePassword(passwordController.text);
+                          //await user?.updateEmail(emailController.text);
+                        }
+
+                        //_submitForm();
                       },
                       child: const Text('update profile'),
                     ),
